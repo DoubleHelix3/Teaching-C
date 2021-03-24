072c09cb486ae633b24fe5ccec3203f44a57621e
Alavi-Ullah <54385477+DoubleHelix3@users.noreply.github.com>
2021-03-24 17:05:28 -0500

    Finished testing code, and set up an excersise

--- Test/test.c
+++ Test/test.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 
 // TODO add support for different OS
-int main(int argc, char **argv) { 
-    system("dir");
-    
+int main(int argc, char **argv) {
+    printf("\nOutput: \n");
+    system("main.exe");
+    printf("\n\nCorrect Output: \n");
+    system("solution.exe");
+    printf("\n\n");
 }
