b3f153664f3012af8dc19b8bdc634d3777c598a4
Alavi-Ullah <54385477+DoubleHelix3@users.noreply.github.com>
2021-03-24 20:36:56 -0500

    made testing local to exercise, and completed L1E1

---
+++ Exercises/L1_Hello_World/E1/internal/test.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef char OS;
+#define windows (OS) 1
+#define mac (OS) 2
+
+OS getOS(int argc, char **argv) {
+    if(argc == 1) return mac;
+    if(!strcmp("windows", *argv)) return windows;
+    if(!strcmp("windows", *argv)) return mac;
+    return 0;
+}
+
+char *readFile() {
+    FILE *fp;
+    char *buff = malloc(255);
+
+    fp = fopen("internal/output.txt", "r");
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("%s\n", buff );
+
+    fclose(fp);
+    return buff;
+}
+
+#define print(x) strcat(printBuffer, x)
+void correctProgram(char *printBuffer) {
+    print("Hello World!");
+}
+
+int main(int argc, char **argv) {
+    OS os = getOS(argc, argv);
+    if(!os) {
+        printf("Not a valid os");
+        return 1;
+    }
+
+    char *executable = (os == windows) ? "./a.out" : "a.exe";
+
+    char *cmd = malloc(50);
+    sprintf(cmd, "%s > internal/output.txt", executable);
+
+    printf("\nyour output:\n");
+    system(cmd);
+
+    char *output = readFile();
+    
+    char *correct = malloc(500);
+    *correct = 0;
+    correctProgram(correct);
+    
+    if(!strcmp(output, correct)) {
+        printf("\nSuccess!\n\n");
+    } else {
+        printf("\ncorrect output: \n%s\n", correct);
+        printf("\nWrong!\n\n");
+    }
+
+    return 0;
+}
